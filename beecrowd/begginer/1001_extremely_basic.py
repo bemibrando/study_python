6f6e340be4e592a8e31c89d16a380e24fdc8d9b2
bemibrando <bemi.brando@outlook.com>
2024-12-24 15:31:38 -0300

    Problem 1001 - Extremely Basic

---
+++ beecrowd/begginer/1001_extremely_basic.py
@@ -0,0 +1,11 @@
+# Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. 
+# Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+
+#  -*- coding: utf-8 -*-
+
+a = int(input())
+b = int(input())
+
+x = a + b
+
+print("X =", x)
