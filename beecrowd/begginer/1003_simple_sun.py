0ba127a991cbe68310ea9e483b2996ff9cf14d23
bemibrando <bemi.brando@outlook.com>
2024-12-25 19:05:31 -0300

    problem 1003 - Simple Sun

---
+++ beecrowd/begginer/1003_simple_sun.py
@@ -0,0 +1,12 @@
+# Read two integer values, in this case, the variables A and B. 
+# After this, calculate the sum between them and assign it to the variable SOMA. 
+# Write the value of this variable.
+
+# -*- coding: utf-8 -*-
+
+a = int(input())
+b = int(input())
+
+sum = a + b
+
+print("SOMA = %d" %sum)
