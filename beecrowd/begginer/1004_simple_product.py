9d0a6eefba68e6dc75f483479cae91453f330eaf
bemibrando <bemi.brando@outlook.com>
2024-12-26 20:22:33 -0300

    Problem 1004 - Simple Product

---
+++ beecrowd/begginer/1004_simple_product.py
@@ -0,0 +1,11 @@
+# Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. 
+# Print the result like the example below. Do not forget to print the end of line after the result, otherwise you will receive “Presentation Error”.
+
+# -*- coding: utf-8 -*-
+
+a = int(input())
+b = int(input())
+
+prod = a * b
+
+print("PROD = %d" %prod)
