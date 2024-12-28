cde8622a75ce125e7201817d3b1b8efecba696ac
bemibrando <bemi.brando@outlook.com>
2024-12-27 21:14:17 -0300

    Problem 1008 - Salary

---
+++ beecrowd/begginer/1007_difference.py
@@ -0,0 +1,13 @@
+# Read four integer values named A, B, C and D. 
+# Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
+
+# -*- coding: utf-8 -*-
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+diff = (a * b - c * d)
+
+print("DIFERENCA = %d" %diff)
